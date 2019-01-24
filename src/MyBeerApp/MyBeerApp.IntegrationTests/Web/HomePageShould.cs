cded7ca9d0d9e66c82d7ac6ae9faee370ea67ac6
Jacob Duijzer <jduijzer@vaa.com>
2019-01-24 21:54:12 +0100

    Added first integration tests for pages

---
+++ src/MyBeerApp/MyBeerApp.IntegrationTests/Web/HomePageShould.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyBeerApp.IntegrationTests.Web
+{
+    public class HomePageShould : IClassFixture<WebTestFixture>
+    {
+        private readonly WebTestFixture _fixture;
+
+        public HomePageShould(WebTestFixture fixture) =>
+            _fixture = fixture;
+
+        [Fact]
+        public async Task ReturnOk()
+        {
+            var response = await _fixture.Client.GetAsync("/");
+            response.Should().NotBeNull();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ src/MyBeerApp/MyBeerApp.IntegrationTests/WebTestFixture.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+
+namespace MyBeerApp.IntegrationTests
+{
+    public class WebTestFixture : IDisposable
+    {
+        public HttpClient Client;
+
+        public WebTestFixture()
+        {
+            var server = new TestServer(new WebHostBuilder()
+                .UseEnvironment(EnvironmentName.Development)
+                .ConfigureTestServices((IServiceCollection serviceCollection) =>
+                {
+                    // Use stubbed database for integration tests
+                    //serviceCollection.AddSingleton<IDatabase<Beer>, StubBeerDatabase>();
+                    //serviceCollection.AddSingleton<IDatabase<Bar>, StubBarDatabase>();
+                })
+                .UseStartup<MyBeerApp.Web.Startup>());
+
+            Client = server.CreateClient();
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
