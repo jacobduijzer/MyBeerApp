897c3d3962d332ba543715cd1e9e594d8ca148ab
Jacob Duijzer <jduijzer@vaa.com>
2019-01-27 23:40:17 +0100

    Configure connection string

--- src/MyBeerApp/MyBeerApp.Infrastructure/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MyBeerApp.Infrastructure
-{
-    public static class Constants
-    {
-        public static string CONNECTION_STRING = @"Server=(localdb)\mssqllocaldb;Database=MyBeerApi;Trusted_Connection=True;ConnectRetryCount=0";
-    }
-}
